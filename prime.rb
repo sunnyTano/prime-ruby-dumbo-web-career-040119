43ef2d4daa9a4d38d1ddb0c43dbf091f74fc8a88
Susan Tan <ssusantan9@gmail.com>
2019-03-25 00:05:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,11 +7,11 @@ end
 true
 end
 
-def prime?(num)
-  n = 2
-  while n < num
-    return false if num % n == 0
-    n += 1
-  end
-  true
-end
+# def prime?(num)
+#   n = 2
+#   while n < num
+#     return false if num % n == 0
+#     n += 1
+#   end
+#   true
+# end
