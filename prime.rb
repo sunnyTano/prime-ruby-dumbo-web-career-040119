217ae835d4094977e054105a154746823c2a5dc1
Susan Tan <ssusantan9@gmail.com>
2019-03-25 00:50:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,11 @@
         return false
     elsif n <= 3
         return true
-    elsif n%2 == 0 || n%3 == 0
+    elsif n % 2 == 0 || n % 3 == 0
         return false
     end
     i = 5
-    while (i*i <= n) do
+    while (i * i <= n) do
         if n % i == 0 or n % (i + 2) == 0
             return false
         end
