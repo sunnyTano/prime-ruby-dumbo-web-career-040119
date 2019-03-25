ce6c4820c98cbf02083b9edd3782086068db1956
Susan Tan <ssusantan9@gmail.com>
2019-03-25 00:04:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,9 @@
-# def prime?(num)
-# i = 2
-# while i < num
-# return false if num % 1 == 0
-#   i++
-# end 
-# true
-# end
-
 def prime?(num)
-  n = 2
-  while n < num
-    return false if num % n == 0
-    n += 1
-  end
-  true
+i = 2
+while i < num
+return false if num % i == 0
+  i++
+end 
+true
 end
+
