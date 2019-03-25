7ee6f3de4ba7f6bd1abe712b556330055350d4fc
Susan Tan <ssusantan9@gmail.com>
2019-03-25 00:08:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,17 @@
-# def prime?(num)
-# i = 2
-# while i < num
-# return false if num % i == 0 || num < 1
-#   i++
-# end 
-# true
-# end
-
 def prime?(num)
-  (2..(num - 1)).each do |n|
-    return false if num % n == 0
-  end
-  true
+i = 2
+while i < num
+return false if num % i == 0 || num < 1
+  i++
+end 
+true
 end
+
+# def prime?(num)
+#   n = 2
+#   while n < num
+#     return false if num % n == 0
+#     n += 1
+#   end
+#   true
+# end
